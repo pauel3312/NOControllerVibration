1949d787de88e013a05ab2ee0329b35e0aa35d06
pauel <pau.lacazedieu@gmail.com>
2026-04-25 16:02:38 +0200

    gun fix+add docs

--- NOCV/Patches/AoAFeedbackPatch.cs
+++ NOCV/Patches/AoAFeedbackPatch.cs
@@ -2,9 +2,16 @@ using HarmonyLib;
 
 namespace NOCV.Patches;
 
+/// <summary>
+///     Patches the AoA feedback class to add vibration feedback.
+/// </summary>
 [HarmonyPatch(typeof(AoAFeedback))]
 public class AoAFeedbackPatch
 {
+    /// <summary>
+    /// Main AoA feedback patch (this function alone is like 80% of the feeling of vibration feedback lol)
+    /// </summary>
+    /// <param name="aircraft"></param>
     [HarmonyPatch(nameof(AoAFeedback.RunAoAFeedback))]
     [HarmonyPostfix]
     public static void AoAFeedbackPostfix(Aircraft aircraft)    
@@ -17,15 +24,15 @@ public class AoAFeedbackPatch
         }
         GameManager.playerInput.SetVibration(1, AoAFeedback.shake*(1/AoAFeedback.aoaEffects.ShakeFactor));
     }
-
-    [HarmonyPatch(nameof(AoAFeedback.SetupAircraft))]
-    [HarmonyPostfix]
-    public static void SetupAircraftPostfix(Aircraft aircraft)
-    {
-        if (aircraft == null) return;
-    //     NOCV.Logger.LogDebug($"{AoAFeedback.aoaEffects.OnsetSpeed}:{AoAFeedback.aoaEffects.FullVolumeSpeed}");
-    //     NOCV.Logger.LogDebug($"{AoAFeedback.aoaEffects.OnsetAlpha}:{AoAFeedback.aoaEffects.FullVolumeAlpha}");
-    //     NOCV.Logger.LogDebug(AoAFeedback.aoaEffects.ShakeFactor);
-    NOCV.Logger.LogDebug(aircraft.name);
-    }
+    
+    // [HarmonyPatch(nameof(AoAFeedback.SetupAircraft))]
+    // [HarmonyPostfix]
+    // public static void SetupAircraftPostfix(Aircraft aircraft)
+    // {
+    //     if (aircraft == null) return;
+    // //     NOCV.Logger.LogDebug($"{AoAFeedback.aoaEffects.OnsetSpeed}:{AoAFeedback.aoaEffects.FullVolumeSpeed}");
+    // //     NOCV.Logger.LogDebug($"{AoAFeedback.aoaEffects.OnsetAlpha}:{AoAFeedback.aoaEffects.FullVolumeAlpha}");
+    // //     NOCV.Logger.LogDebug(AoAFeedback.aoaEffects.ShakeFactor);
+    // NOCV.Logger.LogDebug(aircraft.name);
+    // }
 }
--- NOCV/Patches/GunPatches.cs
+++ NOCV/Patches/GunPatches.cs
@@ -2,13 +2,22 @@ using HarmonyLib;
 
 namespace NOCV.Patches;
 
+/// <summary>
+/// Adds vibration feedback on gunshots 
+/// </summary>
 [HarmonyPatch(typeof(Gun))]
 public class GunPatches
 {
-    [HarmonyPrefix]
+    /// <summary>
+    ///     Vibration feedback on bullet spawn.
+    /// </summary>
+    /// <param name="__instance"></param>
+    /// <param name="timeOffset"></param>
+    [HarmonyPostfix]
     [HarmonyPatch(nameof(Gun.SpawnBullet))]
     public static void SpawnBulletPrefix(Gun __instance, float timeOffset)
     {
+        if (!__instance.attachedUnit.IsLocalPlayer) return;
         GameManager.playerInput.SetVibration(0, 1f, 0.1f, false);
     }
 }
